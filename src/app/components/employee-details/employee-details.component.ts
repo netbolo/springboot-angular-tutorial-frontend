f2c24bdc70aef4f71657171c42b98a52e4692259
netbolo <bolomis@gmail.com>
2021-07-04 11:14:00 +0800

    test completed

---
+++ src/app/components/employee-details/employee-details.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../models/employee';
+import { EmployeeService } from '../../services/employee.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-employee-details',
+  templateUrl: './employee-details.component.html',
+  styleUrls: ['./employee-details.component.css']
+})
+export class EmployeeDetailsComponent implements OnInit {
+
+  id: number;
+  employee: Employee;
+
+  constructor(private route: ActivatedRoute,
+              private employeeService: EmployeeService) {
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params.id;
+
+    this.employee = new Employee();
+    this.employeeService.getEmployeeById(this.id).subscribe(data => {
+      this.employee = data;
+    });
+  }
+
+}
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Employee } from '../models/employee';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private baseURL = 'http://localhost:8080/api/v1/employees';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getEmployeesList(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(`${this.baseURL}`);
+  }
+
+  createEmployee(employee: Employee): Observable<Object> {
+    return this.httpClient.post(`${this.baseURL}`, employee);
+  }
+
+  getEmployeeById(id: number): Observable<Employee> {
+    return this.httpClient.get<Employee>(`${this.baseURL}/${id}`);
+  }
+
+  updateEmployee(id: number, employee: Employee): Observable<object> {
+    return this.httpClient.put(`${this.baseURL}/${id}`, employee);
+  }
+
+  deleteEmployee(id: number): Observable<object> {
+    return this.httpClient.delete(`${this.baseURL}/${id}`);
+  }
+}
